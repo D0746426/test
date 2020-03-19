ab2408857e2913ae0b5602b4d90ac552f74b2f62
D0746426 <56255182+D0746426@users.noreply.github.com>
2020-03-19 14:23:19 +0800

    Update test.java

--- src/test.java
+++ src/test.java
@@ -2,7 +2,7 @@
 public class test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated mehgfhgfhfghgfhthod stub
 		System.out.println("123");
 	}
 
